199b2ef529fe6f69860a60ca81849b94440727a2
c_matt_himes <c.matt.himes@gmail.com>
2025-03-24 13:18:16 -0400

    add tick()

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen_clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
         pygame.display.flip()
+        screen_clock.tick(60)
+        dt = screen_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
